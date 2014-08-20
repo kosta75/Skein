76f6d82e550145add57dbf9e9a6cb4d4dbd55436
csji <seungji222@naver.com>
2014-08-20 14:47:07 +0900

    20140820 16:47

--- Skein/src/main/webapp/resources/js/common.js
+++ Skein/src/main/webapp/resources/js/common.js
@@ -112,6 +112,41 @@ $(document).ready(function() {
 	
 	 });*/
 
+	
+
+	
+	
+	//아이디 찾기, 비밀번호 찾기
+	$("#helpPwdBtn").on("click", function(){
+		if($("#helpPwdEmail").val() == ""){
+			alert("이메일을 입력해주세요");
+		}else if($("#helpPwdName").val() == ""){ 
+			alert("이름을 입력해주세요");
+		}else if($("#helpPwdBirthday").val() == ""){ 
+			alert("생년월일을 입력해주세요");
+		}else{
+			$("#helpPwd").submit();
+		}
+	});
+	$("#helpIdBtn").on("click", function(){
+		 if($("#helpIdName").val() == ""){ 
+			alert("이름을 입력해주세요");
+		}else if($("#helpIdBirthday").val() == ""){ 
+			alert("생년월일을 입력해주세요");
+		}else{
+			$("#helpId").submit();
+		}
+	});
+	if($("#noResult").val() != "" || $("#noResultPwd").val() != "" || $("#emails").val() != "" ||$("#email").val() != "" ||$("#result").val() != ""){
+		$(".form_wrap").css("display", "none");
+		$("#search_result").css("display", "block");
+	}
+	//아이디,비번찾기 끝
+	
+	
+	
+	
+	
 	//상단 메뉴 고정
 	var menupos = $("#menu").offset().top;
 	$(window).scroll(function() {
@@ -128,5 +163,5 @@ $(document).ready(function() {
 			$("#menu2").css("top", "");
 		}
 	});
-
+	
 });
