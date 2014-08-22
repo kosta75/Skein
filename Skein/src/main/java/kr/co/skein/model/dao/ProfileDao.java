fd8de7af524a94bcbb5da4be6d14e0783bc3d8cf
univcss <univcss@gmail.com>
2014-08-22 11:41:50 +0900

    release-1.1.0 - 2014.08.22.11.41.23

--- Skein/src/main/java/kr/co/skein/model/dao/ProfileDao.java
+++ Skein/src/main/java/kr/co/skein/model/dao/ProfileDao.java
@@ -1,10 +1,21 @@
 package kr.co.skein.model.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import kr.co.skein.model.vo.ProfileCommand;
 
 public interface ProfileDao {
 
+	//사용자 프로필 조회
+	public List<ProfileCommand> getMemberProfiles(String email) throws ClassNotFoundException, SQLException;
+	
+	//사용자 프로필 존재여부 확인
+	public int countProfiles(ProfileCommand command) throws ClassNotFoundException, SQLException;
+	
+	//사용자 프로필 추가
+	public int insertProfile(ProfileCommand command) throws ClassNotFoundException, SQLException;
+	
+	//사용자 프로필 업데이트
 	public int updateProfile(ProfileCommand command) throws ClassNotFoundException, SQLException;
 }
--- Skein/src/main/java/kr/co/skein/model/vo/ProfileCommand.java
+++ Skein/src/main/java/kr/co/skein/model/vo/ProfileCommand.java
@@ -2,11 +2,41 @@ package kr.co.skein.model.vo;
 
 public class ProfileCommand {
 
-	private String email;
+	private String profileSeq;
 	private int profileCode;
 	private String profileInfo;
 	private int publicLevelCode;
+	private String email;
 	
-	
+	public String getProfileSeq() {
+		return profileSeq;
+	}
+	public void setProfileSeq(String profileSeq) {
+		this.profileSeq = profileSeq;
+	}
+	public int getProfileCode() {
+		return profileCode;
+	}
+	public void setProfileCode(int profileCode) {
+		this.profileCode = profileCode;
+	}
+	public String getProfileInfo() {
+		return profileInfo;
+	}
+	public void setProfileInfo(String profileInfo) {
+		this.profileInfo = profileInfo;
+	}
+	public int getPublicLevelCode() {
+		return publicLevelCode;
+	}
+	public void setPublicLevelCode(int publicLevelCode) {
+		this.publicLevelCode = publicLevelCode;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 }
